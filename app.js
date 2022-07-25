44fa85b1c23e25edf1a684972e1da13544d05ec6
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-07-25 19:06:37 +0300

    created server, models

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 // Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, BASE_PATH } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
+// подключаем мидлвары, роуты и всё остальное...
 
 app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
---
+++ models/cards.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const cardShema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: ObjectId,
+    required: true,
+  },
+  likes: [{
+    type: ObjectId,
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+
+
+
+});
+
+module.exports = mongoose.model('card', userSchema);
